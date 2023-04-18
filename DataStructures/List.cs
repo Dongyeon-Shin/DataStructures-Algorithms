047c09fd854ff51893fe64fe1d7a308f79b51459
Dongyeon Shin <d.shin0920@gmail.com>
2023-04-19 00:06:53 +0900

    feat: Add와 Capacity를 늘리는 기능 구현
    클래스 내부 배열의 크기를 늘리는 Grow함수와 배열에 값을 차례대로 집어넣고 배열의 크기가 한계에 다다를시 Grow함수를 실행시키는 Add함수 추가

---
+++ DataStructures/List.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    internal class List<T>
+    {
+        private const int DefaultCapacity = 10;
+        private T[] items;
+        private int size;
+        public void Add(T item)
+        {
+            if (size == items.Length)
+            {
+                Grow();
+            }
+            items[size++] = item;
+        }
+        private void Grow()
+        {
+            int newCapacity = items.Length * 2;
+            T[] newItems = new T[newCapacity];
+            Array.Copy(items, 0, newItems, 0, size);
+            items = newItems;
+        }
+    }
+}
