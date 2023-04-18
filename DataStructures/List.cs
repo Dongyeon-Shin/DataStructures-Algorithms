8bfe0b7a1a851fff3d358be3d67f1e3e316e4a3f
Dongyeon Shin <d.shin0920@gmail.com>
2023-04-19 00:16:43 +0900

    feat: Remove, RemoveAt, IndexOf 기능 추가
    배열의 값이 매개변수의 값과 일치하는 인덱스를 찾는 함수와 매개변수로 받은 인덱스로부터 뒤의 데이터들을 복사해와서 해당 인덱스의 자리에 덮어씌우는 함수 그리고 둘을 합쳐놓은 함수 구현

--- DataStructures/List.cs
+++ DataStructures/List.cs
@@ -19,6 +19,32 @@ namespace DataStructures
             }
             items[size++] = item;
         }
+        public bool Remove(T item)
+        {
+            int index = IndexOf(item);
+            if (index >= 0)
+            {
+                RemoveAt(index);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index >= size)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            size--;
+            Array.Copy(items, index + 1, items, index, size - index);
+        }
+        public int IndexOf(T item)
+        {
+            return Array.IndexOf(items, item, 0, size);
+        }
         private void Grow()
         {
             int newCapacity = items.Length * 2;
