d223c02ac39b960a01b6551cb56ffbd3ea0f460d
Dongyeon Shin <d.shin0920@gmail.com>
2023-04-19 17:36:21 +0900

    docs: Linked List에 대한 설명 주석 추가
    // Linked List의 기본 원리와 사용법에 대한 주석

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -66,6 +66,57 @@
         // list의 시간복잡도
         // 접근   탐색  삽입  삭제
         // O(1)   O(n)  O(n) O(n)
+
+
+        // 연결리스트 Linked List
+        // 데이터를 포함하는 노드들을연결식으로 만든 자료구조
+        // 노드는 데이터와 이전/다음 노드 객체를 참조하고 있음
+        // 노드가 메모리에 연속적으로 배치되지 않고 이전/다음노드의 위치를 확인
+
+        // 링크드리스트의 사용
+        void LinkedList()
+        {
+            LinkedList<string> linkedList = new LinkedList<string>();
+            // 링크드리스트 요소 삽입
+            linkedList.AddFirst("0번 앞데이터");
+            linkedList.AddFirst("1번 앞데이터");
+            linkedList.AddLast("0번 뒤데이터");
+            linkedList.AddLast("1번 뒤데이터");
+
+            // 링크드리스트 요소 삭제
+            linkedList.Remove("1번 앞데이터");
+
+            // 링크드리스트 요소 탐색
+            // 노드란 이전주소, 데이터, 다음주소를 담고있는 클래스이다.
+            // 그리고 첫번째이냐 마지막이냐에 따라 이전주소나 다음주소는 없을 수도 있기떄문에
+            // nullable을 이용한다.
+            LinkedListNode<string> findNode = linkedList.Find("0번 뒤데이터");
+
+            // 링크드리스트 노드를 통한 노드 참조
+            LinkedListNode<string> prevNode = findNode.Previous;
+            LinkedListNode<string> nextNode = findNode.Next;
+
+            // 링크드리스트 노드를 통한 노드 삽입
+            linkedList.AddBefore(findNode, "찾은노드 앞데이터");
+            linkedList.AddAfter(findNode, "찾은노드 뒤데이터");
+
+            // 링크드리스트 노드를 통한 삭제
+            linkedList.Remove(findNode);
+        }
+
+        // LinkedList의 시간복잡도
+        // 접근   탐색   삽입   삭제
+        // O(n)   O(n)　 O(1)  O(1)
+        // 인덱스
+        // 사용
+        // 불가능
+
+        // LinkedList의 종류
+        // 단방향 링크드 리스트: 노드에 다음 노드의 주소만 가지고 있는 링크드 리스트
+        // 양방향 링크드 리스트: 노드가 전과 다음 노드의 주소를 모두 가지고 있는 타입의 링크드 리스트
+        // 환(원)형 링크드 리스트: 단방향과 동일하지만 마지막 노드가 첫번째 노드의 주소를 가지고 있는 링크드 리스트
+        // c#의 경우 환형 링크드 리스트 방식이다.
+
         static void Main(string[] args)
         {
             
